c33d182b583da1fdccb0bb59dac7238fb16089d3
Sandrex233 <sandrex233@gmail.com>
2023-02-27 00:25:07 +0400

    fix

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,17 +1,12 @@
 package com.epam.demo;
 
-import com.epam.utils.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
-        for (String str : args){
-            if(!StringUtils.isPositiveNumber(str)){
-                return false;
-            }
-        }
-        return true;
+        return args.stream()
+                .allMatch(arg -> NumberUtils.isCreatable(arg) && NumberUtils.createDouble(arg) > 0);
     }
 }
